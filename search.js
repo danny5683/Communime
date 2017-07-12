4bf14c503a7a41c36223b350e17d0fdc7bd7420e
aokai3 <atahokai@gmail.com>
2017-07-12 10:09:13 -0400

    added search.js

---
+++ search.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Total Community Service Hours</h2>
+
+<p id="demo"></p>
+
+<script>
+var addhours = 5;
+var totalhours = 6;
+var total = addhours + totalhours;
+document.getElementById("demo").innerHTML =
+"The total Community Service Hours is: " + total;
+</script>
+
+</body>
+</html>
