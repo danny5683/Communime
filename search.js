2076010d777f1002dfb51003e3e1a1184faa3ff1
aokai3 <atahokai@gmail.com>
2017-07-12 11:00:10 -0400

    added the right version of search.js

--- search.js
+++ search.js
@@ -1,17 +1,20 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h2>Total Community Service Hours</h2>
-
-<p id="demo"></p>
-
 <script>
-var addhours = 5;
-var totalhours = 6;
-var total = addhours + totalhours;
-document.getElementById("demo").innerHTML =
-"The total Community Service Hours is: " + total;
-</script>
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
 
-</body>
-</html>
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+</script>
