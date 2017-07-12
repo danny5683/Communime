3a6a3314cab8efb68595f84e5121c5dfc9fa341f
VikeP64 <princessvikeilia1@gmail.com>
2017-07-12 13:26:41 -0400

    added total hours

---
+++ totalhours.js
@@ -0,0 +1,9 @@
+
+function myJsFunction(){
+    document.getElementById('demo').innerHTML = date;
+    document.getElementById('nemo').innerHTML = hour;
+    document.getElementById('emo').innerHTML = vents;
+    var date=document.getElementById('datedate').value()
+    var hour=document.getElementById('hours').value()
+    var vents= document.getElementById('event').value()
+    }
