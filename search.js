03528374ab3420706fd50edb17f296d18699a5fd
aokai3 <atahokai@gmail.com>
2017-07-13 10:17:58 -0400

    updated search.html

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function searchFunction() {
     // Declare variables
     var input, filter, ul, li, a, i;
     input = document.getElementById('myInput');
@@ -15,4 +15,26 @@ function myFunction() {
             li[i].style.display = "none";
         }
     }
+}
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
