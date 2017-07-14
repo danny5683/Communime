376a3b38c5d7dd4e9f1495a17089b1ab3f75284e
aokai3 <atahokai@gmail.com>
2017-07-14 10:29:33 -0400

    tweaked addevents and search files

--- addevent.js
+++ addevent.js
@@ -6,5 +6,7 @@ var modal = document.getElementById('id01');
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+
+myFunction
       }
     }
