0d37cb6434f4fea863c40d0d637bdbb77a220acc
VikeP64 <princessvikeilia1@gmail.com>
2017-07-14 16:05:53 -0400

    added css to the group page

---
+++ indexlogin.js
@@ -0,0 +1,9 @@
+function userInList(){
+ var unList = [];
+ var adduser = (document.getElementById('jellybean').value);
+ unList.push(adduser);
+for(i=0; i < unList.length; i++){
+   if (adduser == unList[i])
+    window.alert = ""
+ }
+}
