d904c9c917396bb70227980c00d00e5ab964dacd
Chyna <chynahester2@gmail.com>
2017-07-14 09:54:29 -0400

    took away toolbar

--- trophies.js
+++ trophies.js
@@ -3,5 +3,5 @@
    <img src="tree_grows.gif"width="128" height="128">
 
    <hr>
-   <h1>Thanks for volunteering a total of 10 hours. You get a thumbs up! Keep up the Good Work!
+   <h1>Thanks for volunteering a total of 10 hours.You get a thumbs up! Keep up the Good Work!
    <img src="thumbs_up.jpe"width="128" height="128">
