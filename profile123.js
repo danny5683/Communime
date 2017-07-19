d2c5da9352800bc27a93082f7cf806faf44ba00c
VikeP64 <princessvikeilia1@gmail.com>
2017-07-19 13:37:35 -0400

    added jquery

--- profile123.js
+++ profile123.js
@@ -1,4 +1,4 @@
-
+//src= "new profile.html"
 function goalSubmit(){
 var goal=document.getElementById('few').value;
 document.getElementById('goalpara').innerHTML += "<li>"+goal+"</li>";
@@ -7,3 +7,11 @@ function goalTwo(){
 var goalie=document.getElementById('personal').value;
 document.getElementById('bioo').innerHTML = goalie;
 }
+$(function(){
+  $('#search').click(function(e){
+    e.preventDefault()
+    var getinfo= $('#personal').val()
+      console.log(data)
+      document.getElementById("personal").innerHTML
+  })
+})
